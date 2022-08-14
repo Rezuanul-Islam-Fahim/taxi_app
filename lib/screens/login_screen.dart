6d6c973b6e171e0802e74e9c486e020bef20d901
Rezuanul Islam Fahim <rifahim98@gmail.com>
2022-08-14 23:23:36 +0600

    Unfocus keyboard when click outside

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -10,23 +10,29 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        padding: const EdgeInsets.symmetric(horizontal: 30),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Icon(
-              Icons.location_on_sharp,
-              size: 100,
-              color: Theme.of(context).primaryColor,
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            alignment: Alignment.center,
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  Icons.location_on_sharp,
+                  size: 100,
+                  color: Theme.of(context).primaryColor,
+                ),
+                const SizedBox(height: 10),
+                Text('Taxi App', style: Theme.of(context).textTheme.headline1),
+                const SizedBox(height: 20),
+                const LoginForm(),
+              ],
             ),
-            const SizedBox(height: 10),
-            Text('Taxi App', style: Theme.of(context).textTheme.headline1),
-            const SizedBox(height: 20),
-            const LoginForm(),
-          ],
+          ),
         ),
       ),
     );
