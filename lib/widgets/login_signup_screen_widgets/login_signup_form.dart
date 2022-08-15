0134e011a5ac3adfc8342430a95a40cc649c3cc1
Rezuanul Islam Fahim <rifahim98@gmail.com>
2022-08-15 20:33:39 +0600

    Add username field when signingup

--- lib/widgets/login_signup_screen_widgets/login_signup_form.dart
+++ lib/widgets/login_signup_screen_widgets/login_signup_form.dart
@@ -31,7 +31,7 @@ class _LoginFormState extends State<LoginForm> with SingleTickerProviderStateMix
     authMode = AuthMode.login;
     _animationController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 200),
+      duration: const Duration(milliseconds: 150),
     );
     _sizetransition = CurvedAnimation(
       curve: Curves.easeIn,
@@ -45,7 +45,15 @@ class _LoginFormState extends State<LoginForm> with SingleTickerProviderStateMix
     return Form(
       child: Column(
         children: <Widget>[
-          const SizedBox(height: 15),
+          SizeTransition(
+            sizeFactor: _sizetransition,
+            child: Column(
+              children: [
+                const InputTextField(title: 'Username'),
+                const SizedBox(height: 15),
+              ],
+            ),
+          ),
           const InputTextField(title: 'Email'),
           const SizedBox(height: 15),
           const InputTextField(title: 'Password'),
