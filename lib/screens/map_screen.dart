b5722a1cdcccdb275dfd0b28d13c55b85b21d2c3
Rezuanul Islam Fahim <rifahim98@gmail.com>
2022-08-16 23:30:00 +0600

    CHange mapscreen stateful widget to stateless

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -3,30 +3,18 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:taxi_app/providers/map_provider.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   static const String route = '/home';
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  late MapProvider _mapProvider;
-
-  @override
-  void initState() {
-    _mapProvider = Provider.of<MapProvider>(
+  Widget build(BuildContext context) {
+    Provider.of<MapProvider>(
       context,
       listen: false,
-    );
-    _mapProvider.mapInit();
-    super.initState();
-  }
+    ).mapInit();
 
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       body: Consumer<MapProvider>(
         builder: (BuildContext context, MapProvider mapProvider, _) {
