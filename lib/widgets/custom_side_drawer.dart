d37fae654b066cfe77b0f56f0b4177154801227b
Rezuanul Islam Fahim <rifahim98@gmail.com>
2022-09-04 12:59:27 +0600

    Initialize user account data when app loading up

--- lib/widgets/custom_side_drawer.dart
+++ lib/widgets/custom_side_drawer.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../models/user_model.dart' as user;
+import '../providers/user_provider.dart';
 import '../screens/login_signup_screen.dart';
 import '../screens/map_screen.dart';
 import '../screens/trips_screen.dart';
@@ -11,47 +13,46 @@ class CustomSideDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // final UserProvider userProvider = Provider.of<UserProvider>(
-    //   context,
-    //   listen: false,
-    // );
-    // final user.User? loggedUser = userProvider.loggedUser;
+    final UserProvider userProvider = Provider.of<UserProvider>(
+      context,
+      listen: false,
+    );
+    final user.User? loggedUser = userProvider.loggedUser;
 
     return Drawer(
       child: Column(
         children: [
-          // loggedUser != null
-          //     ?
-          UserAccountsDrawerHeader(
-            currentAccountPicture: Container(
-              decoration: BoxDecoration(
-                color: Colors.grey[50],
-                shape: BoxShape.circle,
-              ),
-              child: const Icon(
-                Icons.person,
-                size: 50,
-                color: Colors.black45,
-              ),
-            ),
-            accountName: Text('Fahim'),
-            accountEmail: Text('rifahim98@gmail.com'),
-          ),
-          // : Container(
-          //     height: 200,
-          //     color: Theme.of(context).primaryColor,
-          //     child: const Center(
-          //       child: CircularProgressIndicator(
-          //         color: Colors.white,
-          //       ),
-          //     ),
-          //   ),
+          loggedUser != null
+              ? UserAccountsDrawerHeader(
+                  currentAccountPicture: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[50],
+                      shape: BoxShape.circle,
+                    ),
+                    child: const Icon(
+                      Icons.person,
+                      size: 50,
+                      color: Colors.black45,
+                    ),
+                  ),
+                  accountName: Text(loggedUser.username!),
+                  accountEmail: Text(loggedUser.email!),
+                )
+              : Container(
+                  height: 200,
+                  color: Theme.of(context).primaryColor,
+                  child: const Center(
+                    child: CircularProgressIndicator(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
           const SizedBox(height: 10),
           _buildButtonTile(
             context: context,
             title: 'Home',
             icon: Icons.home_rounded,
-            onTap: () => Navigator.of(context).pushReplacementNamed(
+            onTap: () => Navigator.of(context).pushNamed(
               MapScreen.route,
             ),
           ),
@@ -66,11 +67,11 @@ class CustomSideDrawer extends StatelessWidget {
             title: 'Logout',
             icon: Icons.exit_to_app,
             onTap: () {
-              // userProvider.clearUser();
-              // Navigator.of(context).pushReplacementNamed(
-              //   LoginSignupScreen.route,
-              // );
-              // FirebaseAuth.instance.signOut();
+              userProvider.clearUser();
+              Navigator.of(context).pushReplacementNamed(
+                LoginSignupScreen.route,
+              );
+              FirebaseAuth.instance.signOut();
             },
           ),
         ],
